dd109d6b6587461615c2a0b4474749f41ceeaa56
Garrett <borng@oregonstate.edu>
2022-01-11 15:26:22 -0500

    Initial Commit

---
+++ Bike Kollective/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bike Kollective
+//
+//  Created by Born4Film on 1/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bike KollectiveUITests/Bike_KollectiveUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bike_KollectiveUITestsLaunchTests.swift
+//  Bike KollectiveUITests
+//
+//  Created by Born4Film on 1/11/22.
+//
+
+import XCTest
+
+class Bike_KollectiveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

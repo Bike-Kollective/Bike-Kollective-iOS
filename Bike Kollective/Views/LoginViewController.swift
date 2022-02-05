d12cc479b0b1e770a692cf191d4e3b9ea92b1e92
kiwonnam2016@u.northwestern.edu <kiwonnam2016@u.northwestern.edu>
2022-02-05 10:31:23 -0600

    google sign in with firebase authentication complete

--- Bike Kollective/Views/LoginViewController.swift
+++ Bike Kollective/Views/LoginViewController.swift
@@ -20,13 +20,41 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    // function to handle Google Sign in on tap of googlesigninbutton
     @IBAction func handleGoogleLogin(_ sender: Any) {
         let googleClientId = FirebaseApp.app()?.options.clientID ?? ""
         let config = GIDConfiguration.init(clientID: googleClientId)
         
-        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { user, error in
-            guard error == nil else { return }
-            guard let user = user else { return }
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+            // guard error == nil else { return }
+            // guard let user = user else { return }
+            
+            if let error = error {
+                print("error")
+                return
+            }
+            
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {return}
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                             accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let error = error {
+                    print("Authentication Error: \(error.localizedDescription)")
+                }
+                
+            }
+            // let emailAddress = user.profile?.email
+            
+            // let firstName = user.profile?.givenName
+            // let lastName = user.profile?.familyName
+            
+            // let prof
+            
         }
         
     }

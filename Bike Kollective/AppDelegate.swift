6a449bdeffe1f09b133715bd9dce8656b5a331bd
Garrett <borng@oregonstate.edu>
2022-02-05 15:27:53 -0500

    Resolved conflicts from Kiwon's pull request.

--- Bike Kollective/AppDelegate.swift
+++ Bike Kollective/AppDelegate.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseCore
+import GoogleSignIn
 import GoogleMaps
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,16 +19,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // GMSServices.provideAPIKey("")
-        // Connects to Firebaseapp
-        FirebaseApp.configure()
-        // Connects to Firestore database
-        let db = Firestore.firestore()
         
-        print(db)
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
         
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
 
+          handled = GIDSignIn.sharedInstance.handle(url)
+          if handled {
+            return true
+          }
+
+          // Handle other custom URL types.
+
+          // If not handled by this app, return false.
+          return false
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
